ff2b6f7a8af9b78ccc02b00206831f47047c912c
Terry <sassyforever@gmail.com>
2017-06-01 18:58:28 +0800

    add TableView test

--- TableViewMichealTest/ViewController.swift
+++ TableViewMichealTest/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    var answerList = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answerList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        return cell
+    }
 
-
+    @IBAction func checkAction(_ sender: UIButton) {
+        if let inputString = guessTextField.text {
+            answerList.insert(inputString, at: 0)
+        }
+    }
 }
 
